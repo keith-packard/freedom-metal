d53826890d17ea8a7118ecf5fa2803bebb3206c7
RajeshVaradharajan <rajeshrv@sifive.com>
2019-08-06 17:06:57 +0530

    Fixed the VTABLE issue

--- metal/drivers/synopsys_i2c.h
+++ metal/drivers/synopsys_i2c.h
@@ -23,20 +23,7 @@ struct __metal_driver_synopsys_i2c;
 // int __metal_driver_synopsys_i2c_get_interrupt_id(struct metal_i2c *i2c);
 
 
-__METAL_DECLARE_VTABLE(__metal_driver_vtable_synopsys_i2c) =
-{
-
-	.i2c.init				= __metal_driver_synopsys_i2c_init,
-	.i2c.write_transfer 	= __metal_driver_synopsys_i2c_write_transfer,
-	.i2c.read_transfer 		= __metal_driver_synopsys_i2c_read_transfer,
-	.i2c.set_address_mode 	= __metal_driver_synopsys_i2c_set_address_mode,
-	.i2c.set_speed_mode 	= __metal_driver_synopsys_i2c_set_speed_mode,
-	.i2c.set_target_address = __metal_driver_synopsys_i2c_set_target_address,
-	//.i2c.get_interrupt_controller = __metal_driver_synopsys_i2c_get_interrupt_controller,
-	//.i2c.get_interrupt_id   = __metal_driver_synopsys_i2c_get_interrupt_id
-};
-
-
+__METAL_DECLARE_VTABLE(__metal_driver_vtable_synopsys_i2c)
 
 struct __metal_driver_synopsys_i2c
 {
